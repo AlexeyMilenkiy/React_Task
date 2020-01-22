f55b3278c869db15be14d5aeb00ae15d6a4f0e9e
AlexeyMilenkiy <alex.milenkiy@gmail.com>
2020-01-22 18:18:09 +0300

    initial

--- credentials.ts
+++ credentials.ts
@@ -1,9 +1,8 @@
 import { Cred } from './src/lib/interfaces'
 
 const credentials: Cred = {
-  PROJECT_ID: 'psychic-surface-265716',
-  CLIENT_ID:
-    '183388165126-2c826a36bkn8vb2u4pa64bd077frgk1t.apps.googleusercontent.com',
-  API_KEY: 'AIzaSyDGFyp87RWRSVQrh-UIfkSKqVoP2DSqCdw'
+  PROJECT_ID: '',
+  CLIENT_ID: '',
+  API_KEY: ''
 }
 export default credentials
